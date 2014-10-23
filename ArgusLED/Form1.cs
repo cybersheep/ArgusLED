2e70c45a0ebf577d884daecf6c98af3ad5622f43
cybersheep <cybersheep@hotmail.com>
2014-10-23 19:46:18 +0100

    form1 modify 3

--- ArgusLED/Form1.cs
+++ ArgusLED/Form1.cs
@@ -14,7 +14,7 @@ using ArgusTV.Recorder.Entities;
 using ArgusTV.Client.Common;
 using System.Diagnostics;
 
-// 23-Oct 2014: minor change for GitHub testing
+// 23-Oct 2014: minor change for GitHub testing v0.2
 
 namespace ArgusLED
 {
